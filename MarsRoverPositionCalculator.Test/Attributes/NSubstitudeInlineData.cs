73a682e2b63b1a889808bc6f5ffa6a85d2a1e4fe
duranserkan <durserk@gmail.com>
2020-10-25 23:44:38 +0300

    initital setup

---
+++ MarsRoverPositionCalculator.Test/Attributes/NSubstitudeInlineData.cs
@@ -0,0 +1,11 @@
+﻿using AutoFixture.Xunit2;
+using Xunit;
+
+namespace MarsRoverPositionCalculator.Test
+{
+	public class NSubstitudeInlineData : CompositeDataAttribute
+	{
+		public NSubstitudeInlineData(params object[] parameters) : base(new InlineDataAttribute(parameters, new NSubstitudeDataAttribute())) { }
+	}
+}
+
