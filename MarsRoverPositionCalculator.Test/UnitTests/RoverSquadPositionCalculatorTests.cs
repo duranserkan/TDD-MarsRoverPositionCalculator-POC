8aca4558c67c63f10cd28cbb1b8f40e8f34a5138
duranserkan <durserk@gmail.com>
2020-10-26 21:03:42 +0300

    RoverSquadPositionCalculator added

---
+++ MarsRoverPositionCalculator.Test/UnitTests/RoverSquadPositionCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Shouldly;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MarsRoverPositionCalculator.Test.UnitTests
+{
+	public class RoverSquadPositionCalculatorTests
+	{
+
+		[Theory]
+		[MemberData(nameof(GetRoverMovementCalculatorInput))]
+		public void RoverMovementCalculatorInput_Should_Be_Created_From_Input_Format(string input, string expectedOutput)
+		{
+			var calculator = new RoverSquadPositionCalculator();
+			var actualOutput = calculator.CalulateLastPositions(input);
+			actualOutput.ShouldBe(expectedOutput);
+		}
+
+		public static IEnumerable<object[]> GetRoverMovementCalculatorInput()
+		{
+			var input =
+				@"5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+";
+			var output = 
+				@"1 3 N
+5 1 E";
+			yield return new object[]
+			{
+				input,
+				output
+			};
+		}
+	}
+}
---
+++ MarsRoverPositionCalculator/RoverSquadPositionCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MarsRoverPositionCalculator.Models;
+
+namespace MarsRoverPositionCalculator
+{
+	public class RoverSquadPositionCalculator
+	{
+		public string CalulateLastPositions(string inputAsString)
+		{
+			var input= RoverMovementCalculatorInput.CreateFromInputFormat(inputAsString);
+			var output = input.RoverPositionAndControlSignals
+				.Select(x => x.RoverPosition.CalculateLastPosition(x.RoverControlSignals))
+				.Select(lastPosition => $"{lastPosition.X} {lastPosition.Y} {lastPosition.Heading.ToShortCode()}")
+				.Aggregate((result, item) => $"{result}{Environment.NewLine}{item}");
+
+			return output;
+		}
+	}
+}
